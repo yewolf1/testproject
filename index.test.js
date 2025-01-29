c144999f32f24a08c62ee7a3a921554e45877428
yewolf1 <daniel.antunesdossantos@my-digital-school.org>
2025-01-29 11:56:06 +0100

    maj3

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const index = require("./index");
 
 test("ajoute 1 + 2 pour ne pas donner 4", () => {
-  expect(index.sum(1, 2)).toBe(4); // Vérifie que sum(1, 2) renvoie bien 3
+  expect(index.sum(1, 2)).not.toBe(4);
 });
 
 test("ajoute 1 + 2 pour donner 3", () => {
